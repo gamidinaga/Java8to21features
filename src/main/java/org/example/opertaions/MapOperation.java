46fa535873efe1e2e97cd72448a137856eef338f
ganta sairam <sairamganta149@gmail.com>
2025-03-08 23:05:41 -0500

    Commit is done by sai

--- src/main/java/org/example/opertaions/MapOperation.java
+++ src/main/java/org/example/opertaions/MapOperation.java
@@ -1,7 +1,7 @@
 package org.example.opertaions;
 public class MapOperation{
     public void mapOperations(){
-       System.out.println("This is Map Class is changed");
+       System.out.println("This is Map Class was changed by sai");
     }
 
 }
